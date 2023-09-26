1d29742bcfe08229b05b5d05b6a768c48cc7e3de
Revanshill86 <Randolph.evanshill@snhu.edu>
2023-09-26 01:16:19 -0500

    Js function startQuiz, ShowQuestion, resetState

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const questions = [
+const questionList = [
 
     {
         question: "What is the capital of Mexico?",
@@ -63,18 +63,63 @@ const nextButton = document.getElementById('next');
 let currentQuestionIndex = 0;
 let score = 0;
 
+//Function to start the quest(reset quiz)
 function startQuiz()
 {
     currentQuestionIndex = 0;
     score = 0;
+    //Reference to the next button
     nextButton.innerHTML = "Next";
+    //Call function to show the question
     showQuestion();
 }
 
 function showQuestion()
 {
-    let currentQuestion = questions[currentQuestionIndex];
+    resetState();
+    let currentQuestion = questionList[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ": " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButton.appendChild(button);
+        if(answer.correct)
+        {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        {
+
+        }
+        
+    });
 }
+
+function resetState()
+{
+    nextButton.style.display = 'none';
+    while(answerButton.firstChild)
+    {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e)
+{
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if(isCorrect)
+    {
+        selectedBtn.classList.add('correct');
+    }else
+    {
+        selectedBtn.classList.add('incorrect');
+    }
+}
+startQuiz();
 
 
 
